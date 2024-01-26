b636a4b9dcedba92438c939c08dc247f007a1526
abdelhady <bodyyousef169@gmail.com>
2024-01-26 09:40:51 +0200

    Add project files.

---
+++ Abdelhady-Ashraf-Portfolio/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Abdelhady_Ashraf_Portfolio.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
